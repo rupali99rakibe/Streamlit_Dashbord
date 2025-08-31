1e564ee84c3c4ef640997ea6c0c40a33d99409d2
rupali suresh rakibe <156112070+rupali99rakibe@users.noreply.github.com>
2025-08-31 23:38:22 +0530

    First Commit

--- dasboard.py
+++ dasboard.py
@@ -18,7 +18,7 @@ if fl is not None:
     st.write(filename)
     df = pd.read_csv(filename, encoding='ISO-8859-1') if filename.endswith(('csv','txt')) else pd.read_excel(filename)
 else:
-    os.chdir(r"D:\Rupali_Personal\Streamlit_dashboard")
+    # os.chdir(r"D:\Rupali_Personal\Streamlit_dashboard")
     df = pd.read_csv("Superstore.csv", encoding='ISO-8859-1')
 
 col1, col2 = st.columns((2))
