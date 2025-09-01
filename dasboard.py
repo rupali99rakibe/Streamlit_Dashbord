b6e772a376c30a163d8de63ae26d9839cae753aa
rupali suresh rakibe <156112070+rupali99rakibe@users.noreply.github.com>
2025-09-01 09:28:48 +0530

    First Commit

--- dasboard.py
+++ dasboard.py
@@ -8,7 +8,6 @@ warnings.filterwarnings("ignore")
 
 st.set_page_config(page_title="Superstore!!!",page_icon=":bar_chart:", layout="wide")
 
-
 st.title(" :bar_chart: Sample Superstore EDA")
 st.markdown('<style>div.block-container{padding-top:1.75rem;}</style>',unsafe_allow_html=True)
 
