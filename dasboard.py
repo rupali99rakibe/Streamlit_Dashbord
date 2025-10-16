3060be35a84b1ef3c49c88a38ed169e67cd69c63
rupali suresh rakibe <156112070+rupali99rakibe@users.noreply.github.com>
2025-10-16 19:52:20 +0530

    Updatee code

--- dasboard.py
+++ dasboard.py
@@ -7,6 +7,7 @@ warnings.filterwarnings("ignore")
 
 
 st.set_page_config(page_title="Superstore!!!",page_icon=":bar_chart:", layout="wide")
+st.title("hi this is a E-Commerce Dashboard")
 
 st.title(" :bar_chart: Sample Superstore EDA")
 st.markdown('<style>div.block-container{padding-top:1.75rem;}</style>',unsafe_allow_html=True)
